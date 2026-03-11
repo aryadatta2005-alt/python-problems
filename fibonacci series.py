0bdcaf1cbcc9ced035a660606d32e429a4d06960
aryadatta2005 <aryadatta2005@gmail.com>
2026-03-11 23:02:45 +0530

    This is the fibonacci series upto n series

---
+++ fibonacci series.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+if __name__ == "__main__":
+    n = int(input("Enter the number of Fibonacci numbers to generate: "))
+    fibonacci(n)
